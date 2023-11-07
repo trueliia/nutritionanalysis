4fca9707c05a8b96321d6ad7f7e41be536f930bb
trueliia <askyulya@gmail.com>
2023-11-06 23:57:10 -0600

    Second commit

--- src/App.js
+++ src/App.js
@@ -56,16 +56,38 @@ function App() {
       <h1>Nutrition Analysis</h1>
       </div>
       <div className='description'>
-      <p>Track your dietary intake easily! Just enter the ingredients separated by comma: "1 cup of milk, 100g of rice", and detailed nutrient analysis will appear on the screen.</p>
+      <p>Track your dietary intake easily! Enter your ingredients and detailed nutrient analysis will appear on the screen.</p>
       </div>
       <div className='container'>
       <form onSubmit={finalSearch}>
-        <input className='search' placeholder='Search...'
+        <input className='search' placeholder='Enter, ex: 100g rice, 1 banana...'
         onChange={myRecipeSearch}/>
         <div className='container'>
         <button type="submit">COMPUTE</button>
         </div>
       </form>
+      </div>
+      <div className='container'>
+          <p className='description'> Entered Ingredients: {wordSubmitted}</p>
+      </div>
+      <div className='container'>
+          {
+            myNutrition && 
+            <table className='table'>
+              <tr>
+                <th>Total Calories</th>
+                <th>Total Fat</th>
+                <th>Total Protein</th>
+                <th>Total Carbs</th>
+              </tr>
+              <tr>
+                <td>{myNutrition.calories} kcal</td>
+                <td>{myNutrition.totalNutrients.FAT.quantity.toFixed(2)} g</td>
+                <td>{myNutrition.totalNutrients.PROCNT.quantity.toFixed(2)} g</td>
+                <td>{myNutrition.totalNutrients.CHOCDF.quantity.toFixed(2)} g</td>
+              </tr>
+            </table>
+          }
       </div>
       <div className='container'>
     <div className='nutritionLabel'>
